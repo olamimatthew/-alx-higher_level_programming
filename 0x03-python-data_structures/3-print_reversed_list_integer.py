bc8fcc57ec1e1c706da3d58cb058abed3f33d34d
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2023-03-12 22:08:41 +0100

    Error fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    for j in range(len(my_list) - 1, -1, -1):
-#        if j == 0:
-#            print("{:d}".format(my_list[j]), end="")
-        print("{:d}".format(my_list[j]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
