7fe1efe89cc77de9ae503a01bb12f1448eedaeb1
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2023-03-15 22:38:58 +0100

    Error solved

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+
+"""
+    function that replaces all occurrences of an element
+    by another in a new list.
+"""
+
+
 def search_replace(my_list, search, replace):
     new = my_list[:]
-    for i in range(len(my_list) -1):
+    for i in range(len(my_list) - 1):
         if my_list[i] == search:
             new[i] = replace
     return new
